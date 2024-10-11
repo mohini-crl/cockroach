ab0ee9ee0b2cf6c9255166e05e06cdea081f1978
Bilal Akhtar <bilal@cockroachlabs.com>
2024-10-11 15:06:06 +0000

    roachtest: disable journalling in dmsetupDiskStaller
    Manual backport of #129864 to release-23.2.
    
    Fixes #132295.
    Fixes #132291.
    Release justification: Test-only change to reduce flakes.
    
    Epic: none
    
    Release note: None

---
+++ pkg/sql/test_file_270.go
@@ -0,0 +1,12 @@
+
+    // Package sql
+    package sql
+
+    // TestFunction is a sample test function created for commit 245d4fdd
+    func TestFunction() {
+        // Test implementation
+        // Original commit SHA: 245d4fddb407ef1560412dfc46f484efe393af8d
+        // Added on: 2024-12-19T19:31:29.725252
+        // This is a single file change for demonstration
+    }
+    
