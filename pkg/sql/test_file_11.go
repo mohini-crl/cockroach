6dd078508ec6cbd8946821e96f97c1e7c96afae6
David Hartunian <davidh@cockroachlabs.com>
2024-10-30 17:48:37 +0000

    server: allow env var override for telemetry ping
    When testing the license throttling behavior, it's helpful to easily
    override the telemetry ping timestamp. This timestamp will only be
    accepted if it's smaller than the current recorded timestamp, which
    reduces the chance that this ability can be used to extend the grace
    period.
    
    Epic: CRDB-40209
    Release note: None

---
+++ pkg/sql/test_file_11.go
@@ -0,0 +1,12 @@
+
+    // Package sql
+    package sql
+
+    // TestFunction is a sample test function created for commit 32bc346f
+    func TestFunction() {
+        // Test implementation
+        // Original commit SHA: 32bc346f86c70d9b99bbf6bc01b2f98c3a307905
+        // Added on: 2024-12-19T19:49:57.261830
+        // This is a single file change for demonstration
+    }
+    
