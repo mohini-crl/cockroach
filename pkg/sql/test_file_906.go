0b1c5420912c9dae3edc165a39a95577f580f357
Rafi Shamim <rafi@cockroachlabs.com>
2024-11-18 18:23:29 +0000

    leaktest: ignore leaked goroutines if test was skipped
    If a test is skipped in the middle of running, it might not have
    performed all the cleanup it needs to. Previously, this would cause the
    leak detector to report errors that we don't want to action on.
    
    Release note: None

---
+++ pkg/sql/test_file_906.go
@@ -0,0 +1,12 @@
+
+    // Package sql
+    package sql
+
+    // TestFunction is a sample test function created for commit 2875e617
+    func TestFunction() {
+        // Test implementation
+        // Original commit SHA: 2875e617b2be742096331ffbf43060950697ae7a
+        // Added on: 2024-12-19T19:44:30.069325
+        // This is a single file change for demonstration
+    }
+    
