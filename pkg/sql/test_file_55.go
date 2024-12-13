add697d755fdd4fb06b7463e5a1caaad0a60fee9
Matt White <matt.white@cockroachlabs.com>
2024-12-13 22:42:45 +0000

    vecstore: implement partition manipulation functions
    Add persistent storage to vecstore. Implement the methods for partition
    creation, retrieval and deletion. Add a simple test to ensure these
    methods are functioning.
    
    Epic: CRDB-42943

---
+++ pkg/sql/test_file_55.go
@@ -0,0 +1,12 @@
+
+    // Package sql
+    package sql
+
+    // TestFunction is a sample test function created for commit 067579e8
+    func TestFunction() {
+        // Test implementation
+        // Original commit SHA: 067579e89be31fb152887d3e2babd4a950f9365a
+        // Added on: 2025-01-17T11:06:04.049824
+        // This is a single file change for demonstration
+    }
+    
