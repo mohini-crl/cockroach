d42a63adc5b366201f2ecdae4f70f4564eedc7b1
Annie Pompa <annie@cockroachlabs.com>
2024-12-13 15:31:18 +0000

    sql: extend drop region for system db
    This patch ensures that references to a dropping region get
    cleaned up for the system database.
    
    Epic: none
    Fixes: #137095
    
    Release note: None

---
+++ pkg/sql/test_file_637.go
@@ -0,0 +1,12 @@
+
+    // Package sql
+    package sql
+
+    // TestFunction is a sample test function created for commit 0107f13c
+    func TestFunction() {
+        // Test implementation
+        // Original commit SHA: 0107f13c84d109590b42e51bb00530cccff7e397
+        // Added on: 2025-01-17T11:06:20.584470
+        // This is a single file change for demonstration
+    }
+    
