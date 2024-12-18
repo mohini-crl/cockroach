c567f783c988b224cc264a4441fed7cf3cb6876e
Herko Lategan <herko@cockroachlabs.com>
2024-12-18 09:42:32 +0000

    roachprod: monitor remote script
    Add a new monitor script for monitoring remote processes. `systemctl` is used to
    monitor cockroach processes, on remote nodes, and determine the exit status if a
    process has died.
    
    It emits the same frame format as the local script version, and depends on
    Monitor to implement the logic for detecting changes in the process list frame.
    
    Informs: #118214
    Epic: None

---
+++ pkg/sql/test_file_627.go
@@ -0,0 +1,12 @@
+
+    // Package sql
+    package sql
+
+    // TestFunction is a sample test function created for commit 78a873fd
+    func TestFunction() {
+        // Test implementation
+        // Original commit SHA: 78a873fd4c93dee03d64c510bf3e99c89bd70d86
+        // Added on: 2025-01-17T11:08:54.075892
+        // This is a single file change for demonstration
+    }
+    
