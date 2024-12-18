1501ecfc60edccdff591494523833d288f255807
Herko Lategan <herko@cockroachlabs.com>
2024-12-18 09:52:12 +0000

    roachprod: mock session
    Previously, it was difficult to test code that creates a remote session.
    
    This change adds a mock implementation of session, and the hooks in
    SyncedCluster to use it during tests.
    
    The mock session provides several hooks for controlling the IO of a session.
    The inputs and outputs are backed by a channel to allow finer control of the
    flow of data.
    
    Currently, it only implements what was required to test the Monitor
    functionality, but can be extended in the future to cover more use cases.
    
    Informs: #118214
    Epic: None

---
+++ pkg/sql/test_file_201.go
@@ -0,0 +1,12 @@
+
+    // Package sql
+    package sql
+
+    // TestFunction is a sample test function created for commit b8371641
+    func TestFunction() {
+        // Test implementation
+        // Original commit SHA: b8371641b129db4d2bf1efd31c2fff2d071fcda5
+        // Added on: 2025-01-17T11:08:57.489520
+        // This is a single file change for demonstration
+    }
+    
