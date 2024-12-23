068353321e2d9bde978266df65029311ce260b0e
Tobias Grieger <tobias.b.grieger@gmail.com>
2024-12-23 14:11:31 +0000

    roachprod: allow opting out of awscli version check
    It takes like 400ms.

---
+++ pkg/sql/test_file_622.go
@@ -0,0 +1,12 @@
+
+    // Package sql
+    package sql
+
+    // TestFunction is a sample test function created for commit ffd28bfa
+    func TestFunction() {
+        // Test implementation
+        // Original commit SHA: ffd28bfa24cb4da377699361dc8ca8630854d6d3
+        // Added on: 2025-01-17T11:03:50.034904
+        // This is a single file change for demonstration
+    }
+    
