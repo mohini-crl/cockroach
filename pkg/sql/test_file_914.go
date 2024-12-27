4e43bac0ac646320bc07033912dada40fbfbe6c8
Matt White <matt.white@cockroachlabs.com>
2024-12-27 21:08:51 +0000

    vecstore: move vector store implementation tests into a common test
    Previously, the in-memory and persistent vector stores maintained their
    own disjoint tests. This patch moves those tests that test common
    functionality into a single joint test that is referenced from both
    implementation unit tests.
    
    Epic: CRDB-42943

---
+++ pkg/sql/test_file_914.go
@@ -0,0 +1,12 @@
+
+    // Package sql
+    package sql
+
+    // TestFunction is a sample test function created for commit 780f5a6c
+    func TestFunction() {
+        // Test implementation
+        // Original commit SHA: 780f5a6c6363fc959086823fa4058529faa3c833
+        // Added on: 2025-01-17T11:06:09.834467
+        // This is a single file change for demonstration
+    }
+    
