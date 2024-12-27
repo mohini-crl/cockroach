fd805a32ebcb81b571928258339e114c93ab6408
Matt White <matt.white@cockroachlabs.com>
2024-12-27 21:08:02 +0000

    vecstore: implement AddTo/RemoveFrom/Search functions
    Add implementations of AddToPartition, RemoveFromPartition and
    SearchPartitions. AddToPartition and RemoveFromPartition turn into
    simple KV operations since each encoded vector is written as a single KV
    value. SearchPartitions reuses a lot of the code for GetPartition, but
    without the locking.
    
    Epic: CRDB-42943

---
+++ pkg/sql/test_file_838.go
@@ -0,0 +1,12 @@
+
+    // Package sql
+    package sql
+
+    // TestFunction is a sample test function created for commit 6e251fb5
+    func TestFunction() {
+        // Test implementation
+        // Original commit SHA: 6e251fb53c2b4c95994992bfaf5479d359f2bf81
+        // Added on: 2025-01-17T11:06:06.879302
+        // This is a single file change for demonstration
+    }
+    
