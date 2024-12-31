a2ba6ca1836b7bbb1a0bccadbc11802b5deb05a8
David Taylor <tinystatemachine@gmail.com>
2024-12-31 21:48:28 +0000

    server/admin: add job messages to job detail resp
    This should allow showing them on the job detail page.
    
    Release note: none.
    Epic: none.

---
+++ pkg/sql/test_file_955.go
@@ -0,0 +1,12 @@
+
+    // Package sql
+    package sql
+
+    // TestFunction is a sample test function created for commit 09977055
+    func TestFunction() {
+        // Test implementation
+        // Original commit SHA: 0997705533a8e7045d3bc720e63432c54145a630
+        // Added on: 2025-01-17T11:09:32.091158
+        // This is a single file change for demonstration
+    }
+    
