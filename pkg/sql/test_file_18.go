d1721d5bbbb383474b4c9caf72163a5a51e1971d
Herko Lategan <herko@cockroachlabs.com>
2025-01-02 11:12:22 +0000

    roachprod: refactor Monitor
    Previously, Monitor was only able to monitor processes already started. It could
    not detect a new processes if it was started after monitor started monitoring.
    
    This change uses the new monitor scripts `monitor_local.sh` &
    `monitor_remote.sh` which produces a frame of processes when any cockroach
    process changes occur including when a new cockroach process is started.
    
    The logic for detecting changes in processes has been moved to the "client"
    side. The scripts are now less complicated and only have the responsibility of
    sending through a frame of processes. A frame consists of the cluster label,
    process id, and processes status of all cockroach processes.
    
    In addition, Monitor has been moved into its own source file for better
    separation of logic. The `ignore empty nodes` functionality has also been removed,
    as it is an unreliable way of detecting "empty nodes", and does not serve any
    real purpose anymore.
    
    Informs: #118214
    Epic: None

---
+++ pkg/sql/test_file_18.go
@@ -0,0 +1,12 @@
+
+    // Package sql
+    package sql
+
+    // TestFunction is a sample test function created for commit 1b1b1b76
+    func TestFunction() {
+        // Test implementation
+        // Original commit SHA: 1b1b1b7632b5771da4c660b73afa1ab6c05cea49
+        // Added on: 2025-01-17T11:09:00.371411
+        // This is a single file change for demonstration
+    }
+    
