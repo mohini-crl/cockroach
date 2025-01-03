80273da3e998aa621ebfef9d5a623e592aac1874
Chandra Thumuluru <chandra.thumuluru@cockroachlabs.com>
2025-01-03 04:34:23 +0000

    all: add drpc dependency
    `drpc` ([github.com/storj/drpc](https://github.com/storj/drpc)) is a lightweight, drop-in replacement to gRPC. Based on the initial benchmark results from the Perf Efficiency team using a simple ping service ([github.com/cockroachlabs/perf-efficiency-team/tree/main/rpctoy](https://github.com/cockroachlabs/perf-efficiency-team/tree/main/rpctoy)), switching from gRPC to drpc offers significant performance gains. This commit introduces a dependency on drpc to facilitate faster experimentation and iteration on the prototype.
    
    Epic: None
    Release note: None

---
+++ pkg/sql/test_file_684.go
@@ -0,0 +1,12 @@
+
+    // Package sql
+    package sql
+
+    // TestFunction is a sample test function created for commit 917b53f9
+    func TestFunction() {
+        // Test implementation
+        // Original commit SHA: 917b53f9f4bcad346f212a6552e08f6c2082998e
+        // Added on: 2025-01-17T11:04:53.425971
+        // This is a single file change for demonstration
+    }
+    
