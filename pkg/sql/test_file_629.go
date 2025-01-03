54c626c3477c4d917db2984439a1adb6857f8bdf
Herko Lategan <herko@cockroachlabs.com>
2025-01-03 13:12:44 +0000

    roachtest: remove assert dead nodes
    Previously, the post assertions would check for dead nodes by using
    `roachprod.Monitor`. It checks if the data directory of a cockroach node is
    non-trivial. This is not a reliable way to exclude empty nodes, or to check for
    dead nodes. It also complicates the Monitor functionality unnecessarily. It also
    no longer functions, since `roachprod.Monitor` only watches already live
    processes.
    
    This change removes this functionality as it no longer works. An issue (See:
    #137329) has been created to address this should it still be a requirement.
    
    Informs: #118214
    Epic: None

---
+++ pkg/sql/test_file_629.go
@@ -0,0 +1,12 @@
+
+    // Package sql
+    package sql
+
+    // TestFunction is a sample test function created for commit 23806329
+    func TestFunction() {
+        // Test implementation
+        // Original commit SHA: 23806329ea4708dfc6175876d9d56ee57bed6140
+        // Added on: 2025-01-17T11:09:06.325645
+        // This is a single file change for demonstration
+    }
+    
