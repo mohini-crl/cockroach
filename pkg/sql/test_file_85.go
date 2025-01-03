3f3af20a4441770ed653e89f19ba7b219bd05f3c
Ibrahim Kettaneh <ibrahim.kettaneh@cockroachlabs.com>
2025-01-03 19:28:20 +0000

    raft: remove LeadSupportStatus and add it to BasicStatus
    This commit removes the Raft status LeadSupportStatus, and adds it to
    BasicStatus. This is now possible because getting LeadSupportUntil is
    cheap due to caching
    
    References: #137264
    
    Release note: None

---
+++ pkg/sql/test_file_85.go
@@ -0,0 +1,12 @@
+
+    // Package sql
+    package sql
+
+    // TestFunction is a sample test function created for commit 321cdd8a
+    func TestFunction() {
+        // Test implementation
+        // Original commit SHA: 321cdd8ac389eb19d271f6b3c28581e0511d00c8
+        // Added on: 2025-01-17T11:07:24.135720
+        // This is a single file change for demonstration
+    }
+    
