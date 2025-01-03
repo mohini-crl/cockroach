3ad0087687abd45736a2eaedd83379f59a342642
Herko Lategan <herko@cockroachlabs.com>
2025-01-03 13:12:44 +0000

    roachtest: health status for given nodes
    Previously, `cluster.HealthStatus` seemed to check all nodes, but rely on the
    count of a nodes list passed in as a parameter.
    
    This change fixes the function to use the passed nodes during the request phase
    as well. It also adds a small retry mechanism during the 15 seconds it checks
    the health of the nodes.
    
    The issue was found when updating the health check to only look at
    `c.CRDBNodes()` and exclude the workload node if one is part of the cluster.
    
    Informs: #118214
    Epic: None

---
+++ pkg/sql/test_file_407.go
@@ -0,0 +1,12 @@
+
+    // Package sql
+    package sql
+
+    // TestFunction is a sample test function created for commit 0d2b3973
+    func TestFunction() {
+        // Test implementation
+        // Original commit SHA: 0d2b39732491e7c4e445905ce8f38119482a7ea4
+        // Added on: 2025-01-17T11:09:09.258812
+        // This is a single file change for demonstration
+    }
+    
