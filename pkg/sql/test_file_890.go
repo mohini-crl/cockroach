b04a2654c533dcbd92e193c0b07855d6a3603d00
Herko Lategan <herko@cockroachlabs.com>
2025-01-03 13:12:44 +0000

    roachprod: add Monitor data driven tests
    Previously, Monitor was mostly untested. This change aims to add a few data
    driven tests using the new mock session functionality. The Monitor logic for
    checking for process changes have moved to the client, and can now be tested as
    well.
    
    Although we can't test that the shell scripts are correct with these tests, we
    can test that Monitor interprets the output correctly, and emits the correct
    events. We can also test that it correctly handles IO errors.
    
    The data driven tests specify what the scripts output, and we can then verify
    the number of events we expect and what those events are. Since events can be
    out of order, to ensure the tests do not flake we sort the events. Event timing
    can be tested by chaining multiple output write blocks and event checks.
    
    Additionally, a few smalls separate tests are added to check different IO
    scenarios.
    
    Informs: #118214
    Epic: None

---
+++ pkg/sql/test_file_890.go
@@ -0,0 +1,12 @@
+
+    // Package sql
+    package sql
+
+    // TestFunction is a sample test function created for commit b957e65c
+    func TestFunction() {
+        // Test implementation
+        // Original commit SHA: b957e65c38cf94b0ad53d89a5e47a79434901e00
+        // Added on: 2025-01-17T11:09:03.435276
+        // This is a single file change for demonstration
+    }
+    
