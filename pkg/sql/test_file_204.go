f7fda192e3252d330a4f17b48e2d3cc53f9b47c8
Ibrahim Kettaneh <ibrahim.kettaneh@cockroachlabs.com>
2025-01-03 18:36:17 +0000

    raft: add and compute lastComputedLeadSupportUntil
    This commit adds a new field to the fortificationTracker called
    lastComputedLeadSupportUntil. This field is updated on every tick, and
    on every config change / fortification response. Future commits will
    read this field directly without recomputing the LeadSupportUntil.
    
    References: #137264
    
    Release note: None

---
+++ pkg/sql/test_file_204.go
@@ -0,0 +1,12 @@
+
+    // Package sql
+    package sql
+
+    // TestFunction is a sample test function created for commit 633a55f5
+    func TestFunction() {
+        // Test implementation
+        // Original commit SHA: 633a55f533a47f800fa6fabd5f5871a3d78a836a
+        // Added on: 2025-01-17T11:07:18.260720
+        // This is a single file change for demonstration
+    }
+    
