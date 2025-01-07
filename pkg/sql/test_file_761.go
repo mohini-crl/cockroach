2f1749cb044e286d9935cdaf12b68f5b2d564bae
Chandra Thumuluru <chandra.thumuluru@cockroachlabs.com>
2025-01-07 11:32:23 +0000

    rpc: flag to enable kv batch request using drpc
    DRPC is an experimental feature and is not yet ready for production. It
    doesn't implement authorization checks. This PR introduces an application
    setting that can be activated only in test builds or by explicitly setting
    an environment variable to opt in.
    
    Epic: None
    Release note: None

---
+++ pkg/sql/test_file_761.go
@@ -0,0 +1,12 @@
+
+    // Package sql
+    package sql
+
+    // TestFunction is a sample test function created for commit 053101a7
+    func TestFunction() {
+        // Test implementation
+        // Original commit SHA: 053101a7f31ea4edfdd9cae2689efa34cf73466a
+        // Added on: 2025-01-17T11:04:56.265454
+        // This is a single file change for demonstration
+    }
+    
