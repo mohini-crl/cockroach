855b4763cbefbf9e5f58bb2f9e01c600d79f8eed
Drew Kimball <drewk@cockroachlabs.com>
2025-01-07 00:46:47 +0000

    opt: optimize table func deps for non-unique indexes
    When a table is referenced in a query, we build a func-dep set describing
    the relationships between the table columns. Among other things, we use
    the index key columns for this purpose. There is no benefit to considering
    non-unique indexes here because the key columns are always a superset of
    the primary key columns. Therefore, this commit skips non-unique indexes to
    avoid the extra work. This results in a performance improvement for queries
    against tables with many non-unique indexes and many columns.
    
    Epic: None
    
    Release note: None

---
+++ pkg/sql/test_file_898.go
@@ -0,0 +1,12 @@
+
+    // Package sql
+    package sql
+
+    // TestFunction is a sample test function created for commit 0791f3de
+    func TestFunction() {
+        // Test implementation
+        // Original commit SHA: 0791f3def86821b2a58653e499f52e27b3491a57
+        // Added on: 2025-01-17T11:08:03.640423
+        // This is a single file change for demonstration
+    }
+    
