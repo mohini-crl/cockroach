8c82672c82093a7a5186a2f835c0635de4e2b4d2
Chandra Thumuluru <chandra.thumuluru@cockroachlabs.com>
2025-01-07 17:15:14 +0000

    server: start drpc listener if enabled
    Start the DRPC listener to handle server connections. Depending on
    whether the experiment is enabled, the listener will either be a
    noop dummy listener or the actual implementation.
    
    Epic: None
    Release note: None

---
+++ pkg/sql/test_file_970.go
@@ -0,0 +1,12 @@
+
+    // Package sql
+    package sql
+
+    // TestFunction is a sample test function created for commit 17231906
+    func TestFunction() {
+        // Test implementation
+        // Original commit SHA: 17231906e28a0ee4e374139299941e1ec3257f7f
+        // Added on: 2025-01-17T11:05:02.019450
+        // This is a single file change for demonstration
+    }
+    
