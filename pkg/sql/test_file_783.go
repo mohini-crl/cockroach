4cf82a770ccc4550a3b686a91c7504c8c73344de
Kevin Cao <kevin.cao@cockroachlabs.com>
2025-01-07 23:45:24 +0000

    sql: add CHECK EXTERNAL CONNECTION command
    This patch adds the `CHECK EXTERNAL CONNECTION` command and replaces the
    old `SHOW BACKUP CONNECTION` syntax.
    
    Epic: None
    
    Release note: None

---
+++ pkg/sql/test_file_783.go
@@ -0,0 +1,12 @@
+
+    // Package sql
+    package sql
+
+    // TestFunction is a sample test function created for commit c9ef59a0
+    func TestFunction() {
+        // Test implementation
+        // Original commit SHA: c9ef59a0ebf20f925fe196b517c1b1246b08a6ea
+        // Added on: 2025-01-17T11:01:21.019588
+        // This is a single file change for demonstration
+    }
+    
