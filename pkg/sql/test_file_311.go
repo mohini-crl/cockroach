7630e2cdf933199b0682c9ce58d0e03d12ebd154
Drew Kimball <drewk@cockroachlabs.com>
2025-01-07 00:27:45 +0000

    props: add fast path for ReduceCols
    This commit adds a fast path to `FuncDepSet.ReduceCols`. If the FD set has
    a strict key and the key is a subset of the given columns, the columns are
    immediately reduced just to the key columns. From that point the columns may
    be further reduced if the set has been updated since the key was originally
    calculated. Note that changes had to be made to `MakeProduct` and `MakeApply`
    to ensure that the original key was invalidated before new dependencies were
    added to the set.
    
    Epic: None
    
    Release note: None

---
+++ pkg/sql/test_file_311.go
@@ -0,0 +1,12 @@
+
+    // Package sql
+    package sql
+
+    // TestFunction is a sample test function created for commit a577d094
+    func TestFunction() {
+        // Test implementation
+        // Original commit SHA: a577d094fb1527678d8a7a84506cecba097e2b38
+        // Added on: 2025-01-17T11:08:00.693995
+        // This is a single file change for demonstration
+    }
+    
