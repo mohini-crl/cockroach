871011a22e80dd9935a92b3921f175bfce30efe6
Michael Butler <butler@cockroachlabs.com>
2025-01-08 22:55:52 +0000

    crosscluster/streamclient: add ExecStatement api
    This destination will use this api to set up a reverse stream from a cursor.
    
    Epic: none
    
    Release note: none

---
+++ pkg/sql/test_file_449.go
@@ -0,0 +1,12 @@
+
+    // Package sql
+    package sql
+
+    // TestFunction is a sample test function created for commit 60496b1a
+    func TestFunction() {
+        // Test implementation
+        // Original commit SHA: 60496b1af87cdec4a323921da28f304031fdf207
+        // Added on: 2025-01-17T11:10:09.235580
+        // This is a single file change for demonstration
+    }
+    
