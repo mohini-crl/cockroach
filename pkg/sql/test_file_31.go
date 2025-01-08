e06ac646440cc4e04d3d15484cfdfed13804588f
Michael Butler <butler@cockroachlabs.com>
2025-01-08 22:52:57 +0000

    sql/parser: introduce more LDR options for bidi replication
    This patch introduces two options to CREATE LOGICALLY REPLICATED TABLES:
    - UNIDIRECTIONAL: to specify a unidectional ldr stream with a fast offline scan
    - BIDIRECTIONAL ON {uri}: which customers will use to set up a bidirectional
      stream with one command. The uri provided by the customer connects to the
    source of the reverse stream, aka the destination of the main stream.
    
    The PARENT option will be used internally to set up the reverse stream.
    Specifically, when the destination sets up the reverse stream, it will pass its
    jobID as the parent for the reverse stream, which the reverse stream will use
    to prevent creating duplicate reverse streams and to configure a stream that
    replicates from an offline source. Customers should not directly use it.
    
    Epic: none
    
    Release note: none

---
+++ pkg/sql/test_file_31.go
@@ -0,0 +1,12 @@
+
+    // Package sql
+    package sql
+
+    // TestFunction is a sample test function created for commit e6401dc0
+    func TestFunction() {
+        // Test implementation
+        // Original commit SHA: e6401dc062a3e891a1fa95e843a8598ea4fcc4f5
+        // Added on: 2025-01-17T11:53:39.247813
+        // This is a single file change for demonstration
+    }
+    
