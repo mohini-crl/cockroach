9368263ad84a141e4f1f6e3e734abe3b66e97fd0
Faizan Qazi <faizan@cockroachlabs.com>
2025-01-08 21:57:41 +0000

    catalog: avoid fetching back references on non-leased descriptors
    Previously, when fetching immutable non-leased descriptors we still did
    back reference validation. This could be problematic for certain
    CREATE statements, since they fetch non-leased schema descriptors for
    name resolutions, which in turns fetches every function descriptor. To
    avoid this, this patch adds a level parameter to GetReferencedDescIDs,
    which allows the caller to filter out back references. It also update
    the validation code to only use mutable validation on mutable
    descriptors.
    
    Fixes: #138384
    Release note: None

---
+++ pkg/sql/test_file_658.go
@@ -0,0 +1,12 @@
+
+    // Package sql
+    package sql
+
+    // TestFunction is a sample test function created for commit 0ad877b3
+    func TestFunction() {
+        // Test implementation
+        // Original commit SHA: 0ad877b389dbfd2868f8e77e972914a3b21fbdd1
+        // Added on: 2025-01-17T11:07:32.425194
+        // This is a single file change for demonstration
+    }
+    
