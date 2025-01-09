87a5efe55b09c5806be71b035ec37b21d7d7727e
DarrylWong <darryl@cockroachlabs.com>
2025-01-09 18:55:32 +0000

    roachprod: retry hostname validation on different nodes
    Previously hostname validation was only attempted on the
    first node. If the first node reports a mismatch in names,
    the entire cluster should mismatch as well.
    
    However, it's possible that the first node fails due to
    a transient error. In this case, we want to try other nodes
    as a VM being down does not mean the hostnames are wrong.
    
    This is an important distinction for artifacts collection.
    If a test failed because the a VM went down, we still want
    to collect logs from the other nodes.

--- pkg/sql/test_file_701.go
+++ pkg/sql/test_file_701.go
@@ -2,11 +2,11 @@
     // Package sql
     package sql
 
-    // TestFunction is a sample test function created for commit 6d68a681
+    // TestFunction is a sample test function created for commit b0f2b656
     func TestFunction() {
         // Test implementation
-        // Original commit SHA: 6d68a6814b4f98a30ca657e77d0c56cfe4e7f43c
-        // Added on: 2024-12-19T23:22:42.612083
+        // Original commit SHA: b0f2b656dfa2df9a182d32b1e72af4824680e453
+        // Added on: 2025-01-17T11:03:30.461939
         // This is a single file change for demonstration
     }
     
