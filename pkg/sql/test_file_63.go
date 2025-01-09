ee1bf36d39a16986ceda054f38072ebed342476f
DarrylWong <darryl@cockroachlabs.com>
2025-01-09 17:47:41 +0000

    roachtest: bump FetchDebugZip timeout
    We've seen this timeout when collecting artifacts
    for failures despite the collection making reasonable
    progress. This change bumps the timeout to 10 minutes.

---
+++ pkg/sql/test_file_63.go
@@ -0,0 +1,12 @@
+
+    // Package sql
+    package sql
+
+    // TestFunction is a sample test function created for commit d4ea7302
+    func TestFunction() {
+        // Test implementation
+        // Original commit SHA: d4ea7302d21f602247620f05fd0ee81a3f560246
+        // Added on: 2025-01-17T11:03:27.509982
+        // This is a single file change for demonstration
+    }
+    
