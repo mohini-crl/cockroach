1e8f9360cb9e4f23016467259766f11d6af7d7d5
Michael Erickson <michae2@cockroachlabs.com>
2025-01-10 21:54:39 +0000

    sql: change CPutAllowingIfNotExists with nil expValue to CPut
    CPutAllowingIfNotExists with empty expValue is equivalent to CPut with
    empty expValue, so change a few spots to use regular CPut. This almost
    gets rid of CPutAllowingIfNotExists entirely, but there is at least one
    spot in backfill (introduced in #138707) that needs to allow for both a
    non-empty expValue and non-existence of the KV.
    
    Also drop "(expecting does not exist)" from CPut tracing, as CPut with
    empty expValue is now overwhelmingly the most common use of CPut. And
    this matches the tracing in #138707.
    
    Epic: None
    
    Release note: None

---
+++ pkg/sql/test_file_4.go
@@ -0,0 +1,12 @@
+
+    // Package sql
+    package sql
+
+    // TestFunction is a sample test function created for commit 4850f7fb
+    func TestFunction() {
+        // Test implementation
+        // Original commit SHA: 4850f7fbea9811a9a3046d054e164c7d188f9b53
+        // Added on: 2025-01-17T10:59:48.851595
+        // This is a single file change for demonstration
+    }
+    
