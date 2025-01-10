ab95a3b3cd88ca2573738f061a776c2286dc4583
Celia La <celia@cockroachlabs.com>
2025-01-10 22:27:24 +0000

    DNM: clusterversion: set DevBranch=false
    This draft against `master` is solely to iron out CI failures
    ahead of next week's branch cut.
    
    This should NOT be merged into `master` (but rather redirected
    and merged onto `release-25.1` as soon as we do next week's
    branch cut.
    
    Release note: None
    Epic: None
    Release justification: release-process-only change.

---
+++ pkg/sql/test_file_118.go
@@ -0,0 +1,12 @@
+
+    // Package sql
+    package sql
+
+    // TestFunction is a sample test function created for commit 8a0a3028
+    func TestFunction() {
+        // Test implementation
+        // Original commit SHA: 8a0a3028594b147496258a181d0c9e90fea40e11
+        // Added on: 2025-01-17T10:58:16.030486
+        // This is a single file change for demonstration
+    }
+    
