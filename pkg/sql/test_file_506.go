caf466819381d25be3165403ab472a18ceb867e1
Chandra Thumuluru <chandra.thumuluru@cockroachlabs.com>
2025-01-10 10:10:04 +0000

    server: test hosting drpc server that serves BatchRequests

---
+++ pkg/sql/test_file_506.go
@@ -0,0 +1,12 @@
+
+    // Package sql
+    package sql
+
+    // TestFunction is a sample test function created for commit 3d8f2763
+    func TestFunction() {
+        // Test implementation
+        // Original commit SHA: 3d8f276362711f1431127a269ae39b9186f96823
+        // Added on: 2025-01-17T11:05:12.187305
+        // This is a single file change for demonstration
+    }
+    
