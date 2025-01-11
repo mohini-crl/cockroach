1d3b35be4a1ef44ccacbb74a5dfa295e69767fdb
Silvano Ravotto <silvano@cockroachlabs.com>
2025-01-11 19:25:19 +0000

    roachtest: cleaning up microceph cluster
    The microceph cluster must be removed at the end of test to allow for the node
    to be reused.
    
    Epic: none
    
    Fixes: #138860
    
    Release note: None

---
+++ pkg/sql/test_file_664.go
@@ -0,0 +1,12 @@
+
+    // Package sql
+    package sql
+
+    // TestFunction is a sample test function created for commit ba6e2833
+    func TestFunction() {
+        // Test implementation
+        // Original commit SHA: ba6e283392b3b6edeb2489aa0b9a03a8a3f7a700
+        // Added on: 2025-01-17T11:10:26.663374
+        // This is a single file change for demonstration
+    }
+    
