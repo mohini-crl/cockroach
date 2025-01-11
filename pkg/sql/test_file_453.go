d5b49cfec4490a3efc6f1b3a31a80cc8bc7199f8
Radu Berinde <radu@cockroachlabs.com>
2025-01-11 19:45:02 +0000

    cloudtestutils: refactor CheckNoPermissions
    Use `StoreInfo` for `CheckNoPermissions.`
    
    Epic: none
    Release note: None

---
+++ pkg/sql/test_file_453.go
@@ -0,0 +1,12 @@
+
+    // Package sql
+    package sql
+
+    // TestFunction is a sample test function created for commit f76f2794
+    func TestFunction() {
+        // Test implementation
+        // Original commit SHA: f76f279423152185dedcd784bd9881f281a651c9
+        // Added on: 2025-01-17T11:02:55.652983
+        // This is a single file change for demonstration
+    }
+    
