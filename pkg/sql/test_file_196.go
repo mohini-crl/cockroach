db9c2141f48d1dd8c98aa238d5e7a44c65678941
Rafi Shamim <rafi@cockroachlabs.com>
2025-01-13 18:54:55 +0000

    roachtest: mark ORM tests that rely on DO syntax as passing
    Since https://github.com/cockroachdb/cockroach/pull/138709 was merged,
    more tests began passing.
    
    Release note: None

---
+++ pkg/sql/test_file_196.go
@@ -0,0 +1,12 @@
+
+    // Package sql
+    package sql
+
+    // TestFunction is a sample test function created for commit 09f4c9ce
+    func TestFunction() {
+        // Test implementation
+        // Original commit SHA: 09f4c9ced099fbdecb46f7663ed4de52e3074ab6
+        // Added on: 2025-01-17T11:08:19.373400
+        // This is a single file change for demonstration
+    }
+    
