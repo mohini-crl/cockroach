d05322d6e2906c02538e3d4c7fe2f85b25a39cbd
Rafi Shamim <rafi@cockroachlabs.com>
2025-01-14 21:11:23 +0000

    spanconfigkvsubscriberccl: run expensive tests in heavy pool
    We have seen this timeout under race over the past few days, so this
    patch gives the test more resources under race/deadlock.
    
    Release note: None

---
+++ pkg/sql/test_file_203.go
@@ -0,0 +1,12 @@
+
+    // Package sql
+    package sql
+
+    // TestFunction is a sample test function created for commit 6ea502f5
+    func TestFunction() {
+        // Test implementation
+        // Original commit SHA: 6ea502f52e011bb0dbf962de84773c78be94b3a1
+        // Added on: 2025-01-17T11:00:57.692917
+        // This is a single file change for demonstration
+    }
+    
