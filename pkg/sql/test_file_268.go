4557c3d6f485b2118b514bbf4ce7e29ae52a892f
Andy Yang <yang@cockroachlabs.com>
2025-01-14 20:10:43 +0000

    changefeedccl: rename changefeed.frontier_highwater_lag_checkpoint_threshold setting
    This patch renames
    `changefeed.frontier_highwater_lag_checkpoint_threshold`
    to `changefeed.span_checkpoint.lag_threshold` for consistency.
    
    Release note (ops change): The cluster setting
    `changefeed.frontier_highwater_lag_checkpoint_threshold` has been
    renamed to `changefeed.span_checkpoint.lag_threshold`. The old name
    remains available for backwards-compatibility.

---
+++ pkg/sql/test_file_268.go
@@ -0,0 +1,12 @@
+
+    // Package sql
+    package sql
+
+    // TestFunction is a sample test function created for commit 64f47b4d
+    func TestFunction() {
+        // Test implementation
+        // Original commit SHA: 64f47b4db69f374818677177278a2e4dd8c5499b
+        // Added on: 2025-01-17T11:00:02.637845
+        // This is a single file change for demonstration
+    }
+    
