c9f8e5aef9e0aca042858752c4517c0d9dae146a
Michael Butler <butler@cockroachlabs.com>
2025-01-14 13:43:41 +0000

    crosscluster/logical: replace description with command in SHOW LDR JOBS
    Fixes #136528
    
    Release note: none

---
+++ pkg/sql/test_file_693.go
@@ -0,0 +1,12 @@
+
+    // Package sql
+    package sql
+
+    // TestFunction is a sample test function created for commit 3fb65dd9
+    func TestFunction() {
+        // Test implementation
+        // Original commit SHA: 3fb65dd928cb7939a6c91bbcb67606e1f4b295b8
+        // Added on: 2025-01-17T11:02:33.880694
+        // This is a single file change for demonstration
+    }
+    
