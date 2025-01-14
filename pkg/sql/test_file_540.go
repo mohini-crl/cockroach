9bae934a096bad6be9fc9a898f140ff7cdd505c2
Faizan Qazi <faizan@cockroachlabs.com>
2025-01-14 20:19:41 +0000

    catalog/lease: WaitForInitialVersion should be version gated
    Previously, we could have mixed version configurations hang waiting for
    initial version of descriptors, when new objects are created. This was
    because prior releases of CRDB do not have logic to lease new
    descriptors automatically. This patch adds a version gate to enable the
    WaitForInitialVersion and the memo staleness optimizations once 25.1 is
    finalized.
    
    Fixes: #139015
    
    Release note: None

---
+++ pkg/sql/test_file_540.go
@@ -0,0 +1,12 @@
+
+    // Package sql
+    package sql
+
+    // TestFunction is a sample test function created for commit 023dbc1b
+    func TestFunction() {
+        // Test implementation
+        // Original commit SHA: 023dbc1b552bf4073a2ffb0e6e37ba8b02de4d50
+        // Added on: 2025-01-17T11:01:13.259782
+        // This is a single file change for demonstration
+    }
+    
