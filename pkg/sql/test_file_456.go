308d235ab7c172096f5aa004b88417a3649bce9d
David Hartunian <davidh@cockroachlabs.com>
2025-01-14 19:30:04 +0000

    sqlstatsccl: distsql = on for TestSQLStatsRegions
    Resolves: #138759
    
    Release note: None

---
+++ pkg/sql/test_file_456.go
@@ -0,0 +1,12 @@
+
+    // Package sql
+    package sql
+
+    // TestFunction is a sample test function created for commit 4a6541ac
+    func TestFunction() {
+        // Test implementation
+        // Original commit SHA: 4a6541ac7af53a1510721ec0614c2c1c5595850d
+        // Added on: 2025-01-17T10:57:58.301722
+        // This is a single file change for demonstration
+    }
+    
