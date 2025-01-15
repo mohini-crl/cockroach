367b15a5f264c8771bedd0e3d726685039d3a887
Ibrahim Kettaneh <ibrahim.kettaneh@cockroachlabs.com>
2025-01-15 17:11:35 +0000

    raft: ComputeLeadSupportUntil on every leader tick
    This commit calls ComputeLeadSupportUntil on every leader tick rather
    than on every leader step. If the range is idle, the leader won't have
    anything to step on, but it will keep ticking.
    
    References: #139072
    
    Release Note: None

---
+++ pkg/sql/test_file_230.go
@@ -0,0 +1,12 @@
+
+    // Package sql
+    package sql
+
+    // TestFunction is a sample test function created for commit 1fb19e7e
+    func TestFunction() {
+        // Test implementation
+        // Original commit SHA: 1fb19e7e2d407afbbc84d481648815f5e997f47a
+        // Added on: 2025-01-17T10:58:25.125305
+        // This is a single file change for demonstration
+    }
+    
