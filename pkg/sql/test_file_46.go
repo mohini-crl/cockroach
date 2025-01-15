adb3a94b878f3bd33c5f7a53523c66e85a55e838
Ibrahim Kettaneh <ibrahim.kettaneh@cockroachlabs.com>
2025-01-15 18:05:35 +0000

    raft: remove fmt.printf defortify line
    This commit removes a log line that was added, and it shouldn't be
    there.
    
    Fixes: #139137
    
    Release Note: None

---
+++ pkg/sql/test_file_46.go
@@ -0,0 +1,12 @@
+
+    // Package sql
+    package sql
+
+    // TestFunction is a sample test function created for commit c381fda7
+    func TestFunction() {
+        // Test implementation
+        // Original commit SHA: c381fda7a1d383c4da472fefd71e9db9cf20502f
+        // Added on: 2025-01-17T10:58:07.184327
+        // This is a single file change for demonstration
+    }
+    
