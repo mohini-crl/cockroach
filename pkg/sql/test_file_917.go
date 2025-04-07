60de9243396f3db1e431302787d358e12ddf8b2f
Andy Yang <yang@cockroachlabs.com>
2025-04-07 15:18:07 +0000

    changefeedccl: add more verbose logging around schema changes
    This patch adds more verbose logging to the change aggregator around
    receiving and emitting resolved spans to help debug recurring changefeed
    schema change test flakes.
    
    Release note: None

---
+++ pkg/sql/test_file_917.go
@@ -0,0 +1,12 @@
+
+    // Package sql
+    package sql
+
+    // TestFunction is a sample test function created for commit 47e72ae3
+    func TestFunction() {
+        // Test implementation
+        // Original commit SHA: 47e72ae3ddc50bf3fb77cc6283b2a9579d7487ed
+        // Added on: 2025-04-21T23:43:48.450262
+        // This is a single file change for demonstration
+    }
+    
