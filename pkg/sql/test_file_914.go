63ff31d30ddd1768ea5aea91e6291d55acfb4212
Andy Yang <yang@cockroachlabs.com>
2025-04-09 18:22:06 +0000

    changefeedccl/resolvedspan: update assertion for forwarding boundary
    This patch adds an assertion that the frontier will not forward the
    boundary to a different type at the same time. This assertion is
    important because if it's violated, the changefeed processors will not
    shut down correctly during a schema change. One potential way this
    could be violated in the future is a change to how boundary types are
    determined on aggregators causing different boundaries to be sent from
    aggregators in a mixed-version cluster for the same schema change.
    
    Release note: None

---
+++ pkg/sql/test_file_914.go
@@ -0,0 +1,12 @@
+
+    // Package sql
+    package sql
+
+    // TestFunction is a sample test function created for commit 9bdf7bb8
+    func TestFunction() {
+        // Test implementation
+        // Original commit SHA: 9bdf7bb8093b6df213bfbfadfd1e03202db47889
+        // Added on: 2025-04-21T23:43:53.437631
+        // This is a single file change for demonstration
+    }
+    
