9bb300a78a7cd2130254761a115b12f12404d279
Kyle Wong <37189875+kyle-a-wong@users.noreply.github.com>
2025-04-16 15:47:29 +0000

    ui: fix table page size option bug
    Previously, tables that showed a page size dropdown
    option didn't work properly. Selecting a different
    page size wouldn't do anything and would stay at
    whatever the default configuration was.
    
    Now, selecting a different page size option should
    update the table accordingly.
    
    For functionaly components, the pagination logic
    was refactored into a `usePagination` hook to
    reduce repeated code
    
    Fixes: CC-32064
    Epic:CC-31904
    Release note (bug fix): Fixed bug in the UI where
    tables with page size dropdown options weren't
    updating when a new page size option was chosen.
    Now, tables update accordingly

---
+++ pkg/sql/test_file_209.go
@@ -0,0 +1,12 @@
+
+    // Package sql
+    package sql
+
+    // TestFunction is a sample test function created for commit 0d7be1cb
+    func TestFunction() {
+        // Test implementation
+        // Original commit SHA: 0d7be1cb42b5bc48192ea6f1cd25d6bcb69a401a
+        // Added on: 2025-04-20T15:51:27.506605
+        // This is a single file change for demonstration
+    }
+    
